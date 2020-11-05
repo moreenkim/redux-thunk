91a218ad1198eb33c0dff46eccb6ced815f73357
Moreen <moreenkim36@gmail.com>
2020-11-05 16:38:41 +0300

    Add the filter changed selector functionality to the posts

--- src/store/posts/reducer.js
+++ src/store/posts/reducer.js
@@ -4,17 +4,46 @@
 // the other exports in this file are selectors, which is business logic that digests parts of the store's state
 // for easier consumption by views
 
+import _ from 'lodash';
+import * as types from './actionTypes';
 import Immutable from 'seamless-immutable';
 
 const initialState = Immutable({
   postsById: undefined,
-  currentFilter: '',
+  currentFilter: 'all',
   currentPostId: undefined,
 });
 
 export default function reduce(state = initialState, action = {}) {
   switch (action.type) {
+    case types.POSTS_FETCHED:
+      return state.merge({
+        postsById: action.postsById,
+      });
+    case types.FILTER_CHANGED:
+      return state.merge({
+        currentFilter: action.filter,
+      });
     default:
       return state;
   }
+}
+
+// selectors
+
+export function getPosts(state) {
+  const currentFilter = state.posts.currentFilter;
+  const postsById = state.posts.postsById;
+  const postsIdArray =
+    currentFilter === 'all'
+      ? _.keys(postsById)
+      : _.filter(
+          _.keys(postsById),
+          (postId) => postsById[postId].topicUrl === currentFilter
+        );
+  return [postsById, postsIdArray];
+}
+
+export function getCurrentFilter(state) {
+  return state.posts.currentFilter;
 }
