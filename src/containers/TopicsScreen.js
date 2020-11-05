beee92a17393fb29bc3270d952183b8b70c29a70
Moreen <moreenkim36@gmail.com>
2020-11-05 12:28:36 +0300

    Edit an error

--- src/containers/TopicsScreen.js
+++ src/containers/TopicsScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-//import './TopicsScreen.css';
+import './TopicsScreen.css';
 
 class TopicsScreen extends Component {
   render() {
