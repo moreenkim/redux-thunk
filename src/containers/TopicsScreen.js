b7e57b79675aa516ef8dc9eb57e07def86f84c09
Moreen <moreenkim36@gmail.com>
2020-11-05 16:02:51 +0300

    Implement more selectors

--- src/containers/TopicsScreen.js
+++ src/containers/TopicsScreen.js
@@ -3,9 +3,60 @@ import { connect } from 'react-redux';
 import './TopicsScreen.css';
 import * as topicsActions from '../store/topics/action';
 import * as topicsSelectors from '../store/topics/reducer';
+import ListView from '../components/ListView';
+import ListRow from '../components/ListRow';
 
 class TopicsScreen extends Component {
-  // view implementation here
+  componentDidMount() {
+    this.props.dispatch(topicsActions.fetchTopics());
+  }
+
+  render() {
+    if (!this.props.rowsById) return this.renderLoading();
+    return (
+      <div className="TopicsScreen">
+        <ListView
+          rowsIdArray={this.props.rowsIdArray}
+          rowsById={this.props.rowsById}
+          renderRow={this.renderRow.bind(this)}
+        />
+        {!this.props.canFinalizeSelection ? (
+          false
+        ) : (
+          <button
+            className="NextScreen"
+            onClick={this.onNextScreenClick.bind(this)}
+          />
+        )}
+      </div>
+    );
+  }
+
+  renderLoading() {
+    return <p>Loading...</p>;
+  }
+
+  renderRow(rowId, row) {
+    const selected = this.props.selectedIdsMap[rowId];
+    return (
+      <ListRow
+        rowId={rowId}
+        onClick={this.onRowClick.bind(this)}
+        selected={selected}
+      >
+        <h3>{row.title}</h3>
+        <p>{row.description}</p>
+      </ListRow>
+    );
+  }
+
+  onRowClick(rowId) {
+    this.props.dispatch(topicsActions.selectTopic(rowId));
+  }
+
+  onNextScreenClick() {
+    this.props.dispatch(topicsActions.finalizeTopicSelection());
+  }
 }
 
 // which props do we want to inject, given the global store state?
@@ -13,6 +64,8 @@ function mapStateToProps(state) {
   return {
     rowsById: topicsSelectors.getTopicsByUrl(state),
     rowsIdArray: topicsSelectors.getTopicsUrlArray(state),
+    selectedIdsMap: topicsSelectors.getSelectedTopicUrlsMap(state),
+    canFinalizeSelection: topicsSelectors.isTopicSelectionValid(state),
   };
 }
 
