a0b0beb343885b58b47b37ee6fd6d2cbd001f453
Moreen <moreenkim36@gmail.com>
2020-11-05 14:26:07 +0300

    Add a selector to confine refactoring to reducer

--- src/store/topics/reducer.js
+++ src/store/topics/reducer.js
@@ -1,18 +1,15 @@
-import * as types from './actionTypes';
-import Immutable from 'seamless-immutable';
-
-const initialState = Immutable({
-  topicsByUrl: undefined,
-  selectedTopicUrls: [],
-});
+import _ from 'lodash';
 
 export default function reduce(state = initialState, action = {}) {
-  switch (action.type) {
-    case types.TOPICS_FETCHED:
-      return state.merge({
-        topicsByUrl: action.topicsByUrl,
-      });
-    default:
-      return state;
-  }
+  // reducer implementation here
+}
+
+// selectors
+
+export function getTopicsByUrl(state) {
+  return state.topics.topicsByUrl;
+}
+
+export function getTopicsUrlArray(state) {
+  return _.keys(state.topics.topicsByUrl);
 }
